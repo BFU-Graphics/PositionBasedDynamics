dd780164ac07cd77d80e3e673fea8611557bb387
XayahP <ycxiang14sep@gmail.com>
2022-01-29 22:05:00 +0800

    fix Mac OS compatibility

--- Course01-minimal-pbd/src/main.cpp
+++ Course01-minimal-pbd/src/main.cpp
@@ -9,15 +9,20 @@
 
 #include "Utils/visualization/viewer.h"
 
+#include "opencv4/opencv2/opencv.hpp"
+
 int main(int argc, char *argv[])
 {
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
 
-    std::string bunny_path   = std::string(PBD_MODEL_DIR) + "bun_zipper.ply";
+    std::string bunny_path = std::string(PBD_MODEL_DIR) + "bun_zipper.ply";
     std::string mitsuba_path = std::string(PBD_TEXTURE_DIR) + "mitsuba.png";
 
     Discregrid::TriangleMesh mesh(bunny_path);
+
+    cv::Mat image = cv::imread(mitsuba_path);
+    cv::imshow("Mitsuba", image);
 
     igl::opengl::glfw::Viewer viewer;
     viewer.load_mesh_from_file(bunny_path);
