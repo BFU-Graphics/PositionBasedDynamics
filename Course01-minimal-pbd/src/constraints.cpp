6f7bd96c83a343453b18d91df0475f18cb2d437c
Xayahp <ycxiang14sep@gmail.com>
2022-02-15 11:41:28 +0800

    fix distance constraint, support openmp

--- Course01-minimal-pbd/src/constraints.cpp
+++ Course01-minimal-pbd/src/constraints.cpp
@@ -41,22 +41,6 @@ pbd_src::DistanceConstraint::DistanceConstraint(Eigen::VectorXd &init_q, const E
 
 bool pbd_src::DistanceConstraint::solve(Eigen::VectorXd &q, const Eigen::SparseMatrix<double> &M_inv, double stiffness)
 {
-//    double tracked_dp = 0;
-//    for (int i = 0; i < Es.size(); ++i)
-//    {
-//        Eigen::Vector6d p = Es[i] * q;
-//        Eigen::Matrix66d M_inv66 = Es[i] * M_inv * Es[i].transpose();
-//
-//        double C = (B * p).norm() - rest_length(i);
-//        Eigen::Vector6d dC = (B.transpose() * B * p) / (B * p).norm();
-//        Eigen::Vector6d dp = -1 * stiffness * M_inv66.transpose() * (C * dC) / (dC.transpose() * M_inv66 * dC);
-//        dq = dq + Es[i].transpose() * dp;
-//
-//        tracked_dp = C;
-//    }
-////    tracked_C = std::sin(simulation_time_);
-//    record(tracked_dp);
-
     omp_set_num_threads(8);
 #pragma omp parallel for
     for (int i = 0; i < Es.size(); ++i)
