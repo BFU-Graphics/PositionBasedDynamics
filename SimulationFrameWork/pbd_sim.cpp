9889b24f2d1b24c814355bde793ce8d7678286ba
XayahP <ycxiang14sep@gmail.com>
2022-02-18 21:29:10 +0800

    fix high fps bug

--- SimulationFrameWork/pbd_sim.cpp
+++ SimulationFrameWork/pbd_sim.cpp
@@ -29,7 +29,7 @@ void HINASIM::PBDSim::simulate_real_dt()
 
     if (rest_time > 0)
     {
-        std::this_thread::sleep_for(std::chrono::microseconds((int) (rest_time * 1000)));
+        std::this_thread::sleep_for(std::chrono::milliseconds(rest_time));
     }
 }
 
